668668aa533449044054a4282cfb5435009f8dcc
Khalil <khalilpati55@gmail.com>
2024-08-16 23:06:32 +0530

    Initialised project using Vite and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="container mx-auto p-24">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+}
+
+export default App;
